e47d24fd625bcbaf5619b8fecf7e0fc620e88529
Dipu Singh <dsingh197@gmail.com>
2023-02-10 17:50:06 +0530

    update

--- src/Pages/Student/MyCourses/VideoPlayer.js
+++ src/Pages/Student/MyCourses/VideoPlayer.js
@@ -1,61 +1,54 @@
 import React from 'react'
 import ReactPlayer from 'react-player/lazy'
 import YouTube from 'react-youtube';
+import {
+    Player, PlayToggle,
+    ControlBar,
+    ReplayControl,
+    ForwardControl,
+    CurrentTimeDisplay,
+    TimeDivider,
+    PlaybackRateMenuButton,
+    VolumeMenuButton
+} from 'video-react';
+// import "node_modules/video-react/dist/video-react.css"; // import css
+// import "../node_modules/video-react/dist/video-react.css";
 
 const VideoPlayer = () => {
 
-    const opts = {
-        height: '390',
-        width: '640',
-        playerVars: {
-            // https://developers.google.com/youtube/player_parameters
-            autoplay: 0,
-            frameborder: 0,
-            controls: 1, //Contrros
-            disablekb: 1, // Keyboard           
-            rel: 0,     //Relevent Video
-
-            modestbranding: 1, rel: 0, showinfo: 0
-        },
-    };
 
     return (
         <>
             <div> This is player </div>
-            <div className='grid grid-cols-12 space-x-10'>
+            <div className='grid grid-cols-12 space-x-5'>
                 <div className='col-span-9'>
-                    <div className='w-full h-screen border'>
-                        <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-                            controls={true}
-                            pip={false}
-                            className="player"
-                            width="100%"
-                            // height="100%"
-                            playing={true}
-                            onContextMenu={(e) => e.preventDefault()}
-                            config={{
-                                youtube: {
-                                    playerVars: { showinfo: false },
-                                    modestBranding: false,
-                                    showInfo: false,
-                                },
-                                facebook: {
-                                    appId: '12345'
-                                }
-                            }}
-                        />
+                    <div className=' border'>
+                        <Player
+                            playsInline
+                            fluid={false}
+                            width={"300px"}
+                            autoPlay
+                            poster="/assets/poster.png"
+                            src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                        >
+                            <ControlBar autoHide={false} disableDefaultControls={true} >
+                                <PlayToggle />
+                                <ReplayControl seconds={10} order={1.1} />
+                                <ForwardControl seconds={30} order={1.2} />
+                                <CurrentTimeDisplay order={4.1} />
+                                <TimeDivider order={4.2} />
+                                <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
+                                <VolumeMenuButton disabled />
+                            </ControlBar>
+                        </Player>
 
-
-                        <YouTube videoId="2g811Eo7K8U" opts={opts} />;
-
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/ysz5S6PUM-U?modestbranding=1&rel=0&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </div>
                 <div className='col-span-3'>
                     <div className='border p-5'>
-                        Playlist Titile
+                        Playlist Title
 
-                        <p>Propgress Autoplay</p>
+                        <p>Progress Autoplay</p>
 
                         <div className='bg-gray-200'>
                             Video 1
